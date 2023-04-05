3a6ea6d5ff0284184aa3dd95ca4c70843c728ab2
Nerijus <krekas@gmail.com>
2023-04-05 10:39:38 +0300

    Show quizzes in the homepage 🏡

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
 use Illuminate\Contracts\View\View;
 
 class HomeController extends Controller
 {
     public function index(): View
     {
-        return view('home');
+        $query = Quiz::withCount(['questions'])
+            ->has('questions')
+            ->when(auth()->guest() || ! auth()->user()->is_admin, function($query) {
+                return $query->where('published', 1);
+            })
+            ->orderBy('id')
+            ->get();
+
+        $public = $query->where('public', true);
+        $registered = $query->where('public', false);
+
+        return view('home', compact('public', 'registered'));
     }
 }
